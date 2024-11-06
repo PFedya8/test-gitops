d584411d7a610ba764e82d8cf854573324ac5013
Fedor <s02210519@gse.cs.msu.ru>
2024-11-06 17:08:05 +0300

    first script to fix packages for kuber lab

---
+++ install.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Define the new CentOS-Base.repo content
+cat << 'EOF' > /etc/yum.repos.d/CentOS-Base.repo
+# CentOS-Base.repo
+
+[base]
+name=CentOS-$releasever - Base
+baseurl=http://vault.centos.org/7.9.2009/os/$basearch/
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+
+[updates]
+name=CentOS-$releasever - Updates
+baseurl=http://vault.centos.org/7.9.2009/updates/$basearch/
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+
+[extras]
+name=CentOS-$releasever - Extras
+baseurl=http://vault.centos.org/7.9.2009/extras/$basearch/
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+
+[centosplus]
+name=CentOS-$releasever - Plus
+baseurl=http://vault.centos.org/7.9.2009/centosplus/$basearch/
+gpgcheck=1
+enabled=0
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7
+EOF
+
+echo "CentOS-Base.repo has been updated successfully."
+
+yum-config-manager --disable kubernetes
+
+# Perform a yum update
+yum update -y
+
+# Clear and reset the terminal to avoid display issues
+clear
+reset
+
+echo "Update completed, Kubernetes repo disabled, and terminal reset successfully."
